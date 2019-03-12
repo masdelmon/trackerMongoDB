50e4d492193d098d7bc9c7d8c1aaa6e11986db0b
Massimiliano <masdelmon@hotmail.it>
2019-03-12 18:25:41 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // console https://stitch.mongodb.com/ under "Clients"
 
 var config = {
-	appId: "TODO: Find this in your MongoDB Stitch console after you create or impormyapp-yetes"
+	appId: "myapp-yetes"
 };
 
 module.exports = config;
